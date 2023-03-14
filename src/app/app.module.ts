ee24007fcda24342d155e1337455ba5525fbfd0c
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-03-14 14:11:31 -0300

    Adicionando diretivas basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 import { FormsModule } from '@angular/forms';
 import { ListThoughtComponent } from './components/list-thought/list-thought.component';
+import { ThoughtComponent } from './components/thoughts/thought/thought.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ListThoughtComponent } from './components/list-thought/list-thought.com
     HeaderComponent,
     FooterComponent,
     CreateThoughtComponent,
-    ListThoughtComponent
+    ListThoughtComponent,
+    ThoughtComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/list-thought/list-thought.component.ts
+++ src/app/components/list-thought/list-thought.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-thought.component.css']
 })
 export class ListThoughtComponent implements OnInit {
-
+  listaPensamentos = [{
+    conteudo: 'Passa informacoes para o componente filho',
+    autoria: 'Componente pai',
+    modelo: 'modelo3'
+  },{
+    conteudo: 'Minha prpriedade é decorada com @Input',
+    autoria: 'Angular',
+    modelo: 'modelo3'
+  },
+  {
+    conteudo: 'Minha prpriedade é decorada com @Input kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.Minha prpriedade é decorada com @Input kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk',
+    autoria: 'Angular',
+    modelo: 'modelo3'
+  }];
   constructor() { }
 
   ngOnInit(): void {
